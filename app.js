15cb9a8984b2500ea5c50eefd399e503375c0247
Shraddha Shetty <shraddhashetty233@gmail.com>
2020-06-24 21:29:02 +0200

    function constructors and data structure

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 // Module -1
 var budgetController = (function() {
 
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+        var Income = function(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+        };
+
+        var data = {
+            allItems: {
+                exp: [],
+                inc: []
+            },
+            totals: {
+                exp: 0,
+                inc: 0
+            }
+        };
+
     
 
 })(); //IIFE
@@ -8,6 +31,7 @@ var budgetController = (function() {
 // Module -2 
 
 var UIController = (function(){
+
 
 
     var DOMStrings = {
