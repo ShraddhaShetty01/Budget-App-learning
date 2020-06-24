2143a8751723bcbb2c1b2416b6f120127edf97f6
Shraddha Shetty <shraddhashetty233@gmail.com>
2020-06-24 21:25:20 +0200

    Revert "adding git init function"
    This reverts commit 4c5074154a1aa1d071b1cd86691add5b1ca78461.

--- app.js
+++ app.js
@@ -1,33 +1,7 @@
 // Module -1
 var budgetController = (function() {
 
-    // Data model for our expenses and income
-
-    // function constructor 
-
-    var Expense = function(id,description,value){
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
-    var Income = function(id,description,value){
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
-
-
-    // Data structure , instead of arrays 
-    var data = {
-        allItems : {
-          exp : [],
-          inc : []
-        },
-        totals: {
-            exp : 0,
-            inc : 0
-        }
-    }
+    
 
 })(); //IIFE
 
@@ -63,7 +37,7 @@ var UIController = (function(){
 
 // Module -3 - Global app controller
 // This controller connects the mod-1 & 2
-var controller = (function(budgetCtrl, UICtrl){
+var Controller = (function(budgetCtrl, UICtrl){
 
     var setupEventListeners = function() {
 
@@ -126,4 +100,4 @@ var controller = (function(budgetCtrl, UICtrl){
 
 // Separation of Concern - Budgetcontroller and UI controllers
 
-controller.init();
+Controller.init();
