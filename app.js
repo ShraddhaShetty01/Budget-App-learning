7d5e0a66fac205bdc9f05eab28504f09b0d52ac9
Shraddha Shetty <shraddhashetty233@gmail.com>
2020-07-07 23:48:38 +0200

    Displaying the current month and year

--- app.js
+++ app.js
@@ -186,7 +186,8 @@ var UIController = (function(){
         expenseLabel : '.budget__expenses--value',
         percentageLabel : '.budget__expenses--percentage',
         container : '.container',
-        expensesPercLabel : '.item__percentage'
+        expensesPercLabel : '.item__percentage',
+        dateLabel : '.budget__title--month'
 
     };
 
@@ -307,6 +308,21 @@ var UIController = (function(){
                 }
             });
 
+        },
+
+        displayMonth : function(){
+
+            var now,month,year;
+
+            now = new Date();
+
+            months = ['January', 'Febraury', 'March', 'April','May','June','July','August','September','October','November','December'];
+
+            month = now.getMonth();
+
+            year = now.getFullYear();
+
+            document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
         },
 
         
@@ -433,6 +449,7 @@ var Controller = (function(budgetCtrl, UICtrl){
         return {
             init : function() {
                 console.log('Application has started');
+                UICtrl.displayMonth();
                 UICtrl.displayBudget({
                     budget : 0,
                     totalInc : 0,
